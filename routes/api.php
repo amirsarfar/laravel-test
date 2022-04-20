033486bd1eca350dc4b146169e0f8c25c42ff29f
Amir Sarfarazi <amirsarfar@gmail.com>
2022-04-20 16:55:12 +0430

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'v1'], function() {
+
+    // Protected Routes
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+
+    // Public Routes
+    Route::get('/login', function(Request $request) {
+        $token = User::find(1)->createToken('ma-token');
+        return ['token' => $token->plainTextToken];
+    });
+});
