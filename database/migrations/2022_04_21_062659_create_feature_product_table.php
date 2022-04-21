5e4a3d0bf25859c4a1482ec12599e9aea44a933f
Amir Sarfarazi <amirsarfar@gmail.com>
2022-04-21 11:12:44 +0430

    Created models and migrations needed for the store part

---
+++ database/migrations/2022_04_21_062659_create_feature_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feature_product', function (Blueprint $table) {
+            $table->foreignId('feature_id');
+            $table->foreignId('product_id');
+            $table->bigInteger('extra_cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feature_product');
+    }
+};
