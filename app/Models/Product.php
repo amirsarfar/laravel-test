92339c37ba39480fdbb798a1055df1f059884ad3
Amir Sarfarazi <amirsarfar@gmail.com>
2022-04-22 21:10:50 +0430

    Playing around with relationships and database models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,26 @@ class Product extends Model
         'description',
         'price'
     ];
+
+    public function product_attributes()
+    {
+        return $this->belongsToMany(Attribute::class, AttributeProduct::class);
+    }
+
+    public function get_product_specifications_attribute()
+    {
+        $specs = $this->specifications;
+        $category_specs = $this->categories()->with('specifications')->get()->pluck('specifications')->collapse();
+        return $category_specs;
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, CategoryProduct::class);
+    }
+
+    public function specifications()
+    {
+        return $this->belongsToMany(Specification::class, ProductSpecification::class);
+    }
 }
